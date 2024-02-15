348f8c0dd0d0c2c537974b6182b678d520031cc8
Fernando <fe.bonifacio@hotmail.com>
2024-02-14 22:06:16 -0300

    Finalizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+//Podemos puxar de uma API
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://cdn.pixabay.com/photo/2017/05/26/15/07/joystick-2346237_1280.jpg",
+    "title": "GAMES MUDAM VIDAS",
+    "description": "Os games oferecem uma variedade de benefícios que podem melhorar significativamente a qualidade de vida. Eles estimulam o desenvolvimento cognitivo, como habilidades de resolução de problemas e raciocínio rápido. Além disso, os jogos podem promover a socialização, permitindo que as pessoas se conectem e interajam em comunidades virtuais. A experiência de jogar também pode ser uma forma eficaz de reduzir o estresse e proporcionar momentos de relaxamento. Além disso, alguns jogos educacionais podem ajudar no aprendizado de novas habilidades e conhecimentos. Em resumo, os games têm o potencial de serem uma fonte valiosa de entretenimento e crescimento pessoal.",
+
+  },
+  {
+    "id": "2",
+    "photo": "https://cdn.pixabay.com/photo/2020/05/26/03/17/super-mario-5221243_1280.jpg",
+    "title": "Excesso",
+    "description": "O excesso de games pode trazer alguns desafios para indivíduos de todas as idades. Quando os jogos consomem muito tempo, podem levar à negligência de responsabilidades importantes, como trabalho, estudos e relacionamentos pessoais. Além disso, o uso excessivo de games pode levar a problemas de saúde, como sedentarismo, má postura e distúrbios do sono. Em casos extremos, o vício em jogos pode interferir significativamente na vida diária e causar problemas psicológicos, como ansiedade e depressão. É importante encontrar um equilíbrio saudável entre jogar e outras atividades, estabelecendo limites de tempo e priorizando o bem-estar físico e emocional.",
+  },
+  {
+    "id": "3",
+    "photo": "https://cdn.pixabay.com/photo/2021/02/16/18/55/gamer-6022003_1280.png",
+    "title": "Crianças",
+    "description": "Os games desempenham um papel significativo no desenvolvimento das crianças, proporcionando uma série de benefícios cognitivos, sociais e emocionais. Eles estimulam a criatividade e o pensamento crítico, ajudando as crianças a resolver problemas de forma eficaz. Além disso, os jogos podem melhorar as habilidades motoras e a coordenação, especialmente aqueles que requerem movimentos precisos e rápidos. Os games também promovem a socialização, seja jogando com amigos ou interagindo em comunidades online, ajudando as crianças a desenvolverem habilidades de trabalho em equipe e comunicação. Além disso, os jogos podem ser uma fonte de entretenimento educativo, ensinando conceitos como matemática, ciência e história de forma envolvente e interativa. Em resumo, os games são uma ferramenta versátil e poderosa que pode contribuir positivamente para o crescimento e desenvolvimento das crianças.",
+  },
+  {
+    "id": "4",
+    "photo": "https://cdn.pixabay.com/photo/2022/01/12/12/30/nintendo-6932787_1280.jpg",
+    "title": "Games Antigos",
+    "description": "Os games antigos são verdadeiros tesouros nostálgicos que marcaram época e ainda encantam os corações dos jogadores. Com gráficos simples, mas cheios de charme, eles nos transportam para uma era em que a imaginação era o principal motor da diversão. Desde os clássicos como Pac-Man e Tetris até os icônicos Super Mario Bros e Sonic the Hedgehog, esses jogos têm um lugar especial em nossos corações. Além de serem divertidos, os games antigos também ensinaram valiosas lições de perseverança e paciência, já que muitas vezes exigiam habilidades precisas e estratégicas para serem dominados. Reviver essas experiências é como fazer uma viagem no tempo, relembrando uma era de inocência e pura diversão digital.",
+  }
+]
